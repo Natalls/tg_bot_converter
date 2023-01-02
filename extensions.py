f04081ac0a00af5c4134f6472c5f6648b93aa29f
Natalls <118367492+Natalls@users.noreply.github.com>
2023-01-02 15:51:02 +0300

    Add files via upload

---
+++ extensions.py
@@ -0,0 +1,28 @@
+import requests
+import json
+from config import keys
+
+class APIException(Exception):
+    pass
+
+class Converter:
+    @staticmethod
+    def get_price(quote: str, base: str, amount: str):
+        if quote == base:
+            raise APIException(f'Невозможен перевод одинаковой валюты {base}')
+        try:
+            quote_ticker = keys[quote.lower()]
+        except KeyError:
+            raise APIException(f'Не удалось обработать валюту {quote}')
+        try:
+            base_ticker = keys[base.lower()]
+        except KeyError:
+            raise APIException(f'Не удалось обработать валюту {base}')
+        try:
+            amount = float(amount)
+        except ValueError:
+            raise APIException(f'Не удалось обработать количество {amount}, введите число')
+
+        r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={quote_ticker}&tsyms={base_ticker}')
+        total_base = json.loads(r.content)[keys[base.lower()]] * amount
+        return total_base
